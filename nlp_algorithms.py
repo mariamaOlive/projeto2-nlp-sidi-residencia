eb3c4dcdb0a14e2f0c761420fe197376aa393364
Mariama Oliveira <mariama.oliveira@yahoo.com.br>
2022-02-15 17:09:26 -0300

    Adicionando tf-idf no pipeline

--- nlp_algorithms.py
+++ nlp_algorithms.py
@@ -1,4 +1,5 @@
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 
@@ -41,9 +42,49 @@ def apply_bow(df, len_pipeline):
 
     return df_bow
 
+
+
+###TF-IDF###
+
+def calculate_tf_idf(df, column1, column2):
+    
+    data = join_docs(df, column1, column2)
+      
+    tf_idf = TfidfVectorizer().fit_transform(data)
+    
+    return tf_idf
+
+def get_tf_idf(tf_idf, index):
+    
+    index1 = 2*index
+    index2 = 2*index + 1
+    
+    cosine_similarities = cosine_similarity(tf_idf[index1], tf_idf[index2])
+    
+    return cosine_similarities[0][0]
+
+def apply_tf_idf(df, len_pipeline):
+    
+    df_tf_idf = pd.DataFrame()
+
+    for index in range(len_pipeline):
+    
+        tf_idf = calculate_tf_idf(df, f'doc1_pipeline{index}', f'doc2_pipeline{index}')
+
+        tf_idf_list = []
+        for index_df in range(len(df)):
+            tf_idf_list.append(get_tf_idf(tf_idf, index_df))
+        
+        df_tf_idf[f'tf_idf{index}'] = tf_idf_list
+    
+    return df_tf_idf
+        
+
 ''' 
     for param, index in zip(pre_processing_list, range(len(pre_processing_list))):
         
         df[f"doc1_pipeline{index}"] = df["doc1"].apply(lambda x: pre_process(x, **param))
         df[f"doc2_pipeline{index}"] = df["doc2"].apply(lambda x: pre_process(x, **param))
 '''
+
+
